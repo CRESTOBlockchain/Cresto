ba2176f9db241aed647e24acce21a4ae482d9e6c
ottokafka <ottokafka@yahoo.com>
2018-11-17 21:59:31 +0800

    css

--- client/src/store.js
+++ client/src/store.js
@@ -19,7 +19,7 @@ const store = createStore(
 
     // One more step to setup Chrome dev tools uncomment to use below
 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
